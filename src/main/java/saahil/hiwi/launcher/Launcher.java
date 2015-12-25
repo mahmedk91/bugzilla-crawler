d74a41bc878ea856afa248f7a021dc8371aadf5e
Ahmed <mahmedk91@gmail.com>
2015-12-25 07:08:14 +0100

    New console messages given by Launcher while processing data.

--- src/main/java/saahil/hiwi/launcher/Launcher.java
+++ src/main/java/saahil/hiwi/launcher/Launcher.java
@@ -12,32 +12,32 @@ import saahil.hiwi.entities.Bug;
 public class Launcher {
   public static int totalPendingBugs;
   public static int bugsProcessed;
+  static ResultSet bugs;
+  static ResultSet totalBugsCount;
+  static ResultSet pendingBugsCount;
   public static DB db = new DB();
 
   public static void main(String[] args) throws SQLException, IOException {
     System.setProperty("javax.net.ssl.trustStore", Config.SSL_TRUSTSTORE);
-    ResultSet bugs = null, totalBugsCount = null, pendingBugsCount = null;
     String dataFolder;
     String baseURL = null;
     if (args.length % 2 != 0) {
-      System.out.println("Wrong number of arguments." + "\nProgram needs atleast baseURL to run."
-          + "\nCorrect usage is java saahil.hiwi.crawler.Launcher \"baseURL\" \"csvFolder\""
-          + "\nFor sqlite there can be only baseURL");
+      System.out.println("Wrong number of arguments. Use -\n"
+          + "-f     folderlocation -----> to upload bugs from csv\n"
+          + "-url   baseUrl        -----> to crawl specific project");
       System.exit(0);
     } else if (args.length == 2) {
       if (args[0].equals("-f") && !Config.DB_TYPE.equals("sqlite")) {
         dataFolder = args[1];
         uploadBugs(dataFolder);
-        bugs = db.getPendingBugs();
-        totalBugsCount = db.getTotalBugsCount();
-        pendingBugsCount = db.getPendingBugsCount();
+        collectBugsAndMetaData();
       } else if (args[0].equals("-url")) {
         baseURL = args[1];
-        bugs = db.getPendingBugs(baseURL);
-        totalBugsCount = db.getTotalBugsCount(baseURL);
-        pendingBugsCount = db.getPendingBugsCount(baseURL);
+        collectBugsAndMetaData(baseURL);
       } else {
-        System.out.println("Wrong arguments");
+        System.out.println("Wrong arguments. Use -\n"
+          + "-f     folderlocation -----> to upload bugs from csv\n"
+          + "-url   baseUrl        -----> to crawl specific project");
         System.exit(0);
       }
     } else if (args.length == 4 && !Config.DB_TYPE.equals("sqlite")) {
@@ -45,24 +45,20 @@ public class Launcher {
         dataFolder = args[1];
         baseURL = args[3];
         uploadBugs(dataFolder);
-        bugs = db.getPendingBugs(baseURL);
-        totalBugsCount = db.getTotalBugsCount(baseURL);
-        pendingBugsCount = db.getPendingBugsCount(baseURL);
+        collectBugsAndMetaData(baseURL);
       } else if (args[0].equals("-url") && args[2].equals("-f")) {
         baseURL = args[1];
         dataFolder = args[3];
         uploadBugs(dataFolder);
-        bugs = db.getPendingBugs(baseURL);
-        totalBugsCount = db.getTotalBugsCount(baseURL);
-        pendingBugsCount = db.getPendingBugsCount(baseURL);
+        collectBugsAndMetaData(baseURL);
       } else {
-        System.out.println("Wrong arguments");
+        System.out.println("Wrong arguments.Use -\n"
+          + "-f     folderlocation -----> to upload bugs from csv\n"
+          + "-url   baseUrl        -----> to crawl specific project");
         System.exit(0);
       }
     } else {
-      bugs = db.getPendingBugs();
-      totalBugsCount = db.getTotalBugsCount();
-      pendingBugsCount = db.getPendingBugsCount();
+      collectBugsAndMetaData();
     }
     pendingBugsCount.next();
     totalBugsCount.next();
@@ -73,13 +69,40 @@ public class Launcher {
     while (bugs.next()) {
       Bug bug = new Bug(bugs.getInt("BUG_ID"), bugs.getString("BUGZILLA_PRODUCT"));
       ShowBugParser.parse(bug, db);
-      bugsProcessed--;
+      bugsProcessed++;
     }
     bugs.close();
   }
 
+  private static void collectBugsAndMetaData() throws SQLException {
+    System.out.println("\nCollecting bugs and meta data...");
+    bugs = db.getPendingBugs();
+    totalBugsCount = db.getTotalBugsCount();
+    pendingBugsCount = db.getPendingBugsCount();
+    System.out.println("\n-----------------------\nCrawling - All Projects\n-----------------------");
+  }
+
+  private static void collectBugsAndMetaData(String baseURL) throws SQLException {
+    System.out.println("\nCollecting bugs and meta data...");
+    bugs = db.getPendingBugs(baseURL);
+    totalBugsCount = db.getTotalBugsCount(baseURL);
+    pendingBugsCount = db.getPendingBugsCount(baseURL);
+    System.out.println();
+    for (int i=0;i<baseURL.length()+11;i++){
+      System.out.print("-");
+    }
+    System.out.print("\nCrawling - "+baseURL+"\n");
+    for (int i=0;i<baseURL.length()+11;i++){
+      System.out.print("-");
+    }
+    System.out.println();
+  }
+
   private static void uploadBugs(String dataFolder) throws SQLException, IOException {
+    System.out.println("-----------\nBugs Import\n-----------");
+    System.out.println("Dropping constraints for faster import..");
     System.out.println("Begin importing bugs...");
+    db.dropConstraints();
     File f = new File(dataFolder);
     FilenameFilter textFilter = new FilenameFilter() {
       public boolean accept(File dir, String name) {
@@ -94,6 +117,8 @@ public class Launcher {
           .println("Processing... [" + fileCount + "/" + files.length + "] - " + file.getName());
       db.importBugs(file.getCanonicalPath());
     }
+    System.out.println("Re-creating constraints...");
+    db.reCreateConstraints();
     System.out.println("Bugs successfully imported!");
   }
 
